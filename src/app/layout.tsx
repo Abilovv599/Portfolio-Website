40e9461f66537af318f31a84e38ba95f8e246063
Jeyhun Abilov <abilovv599@gmail.com>
2024-03-04 00:28:57 +0400

    add icons and navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,9 +23,9 @@ export default function RootLayout({
       >
         <div className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
-        {/* <Header /> */}
+        <Header />
         {children}
-        {/* <Footer /> */}
+        <Footer />
       </body>
     </html>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,9 @@
 
 import { FC, ComponentPropsWithoutRef } from "react";
 import { motion } from "framer-motion";
+import { links } from "@/data/data";
+import Link from "next/link";
+import clsx from "clsx";
 
 interface HeaderProps extends ComponentPropsWithoutRef<"header"> {}
 
@@ -14,7 +17,25 @@ const Header: FC<HeaderProps> = ({ ...props }) => {
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
       <nav className="fixed left-1/2 top-[0.15rem] flex h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5"></ul>
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+          {links.map((link) => (
+            <motion.li
+              className="relative flex h-3/4 items-center justify-center"
+              key={link.hash}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+            >
+              <Link
+                className={clsx(
+                  "flex w-full items-center justify-center px-3 py-3 transition hover:text-gray-950 dark:text-gray-500 dark:hover:text-gray-300",
+                )}
+                href={link.hash}
+              >
+                {link.name}
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
       </nav>
     </header>
   );
