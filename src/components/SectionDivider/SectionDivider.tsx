cbb96b49100153a7d56a81c3bc3732f204fb9979
Jeyhun Abilov <abilovv599@gmail.com>
2024-03-04 19:06:10 +0400

    add props to SectionDivider component

--- src/components/SectionDivider/SectionDivider.tsx
+++ src/components/SectionDivider/SectionDivider.tsx
@@ -12,6 +12,7 @@ const SectionDivider: FC<SectionDividerProps> = ({ ...props }) => {
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.125 }}
+      {...props}
     ></motion.div>
   );
 };
