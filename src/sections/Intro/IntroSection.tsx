f75d8d31f0e5ec48b0926cb8c7851b73145137b4
Jeyhun Abilov <abilovv599@gmail.com>
2024-03-06 19:33:27 +0400

    add image optimizer

--- src/sections/Intro/IntroSection.tsx
+++ src/sections/Intro/IntroSection.tsx
@@ -34,11 +34,11 @@ const IntroSection: FC<IntroSectionProps> = ({ ...props }) => {
               height="192"
               quality="100"
               priority={true}
-              className="h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
+              className="h-40 w-40 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
             />
           </motion.div>
           <motion.span
-            className="absolute bottom-0 right-0 text-4xl"
+            className="absolute bottom-0 right-[6px] text-5xl"
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
