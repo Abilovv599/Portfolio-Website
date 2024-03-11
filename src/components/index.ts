0119e220b1bbcf817b4cf40ceb413092a8306845
Abilov599 <abilovv599@gmail.com>
2024-03-11 14:18:28 +0400

    create SubmitButton component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { Project } from "./Project";
 export { SectionDivider } from "./SectionDivider";
 export { SectionHeading } from "./SectionHeading";
 export { Skill } from "./Skill";
+export { SubmitButton } from "./SubmitButton";
