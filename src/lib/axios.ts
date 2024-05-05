bb1f493867ebc4c907aa49b5e89390da888d9030
Abilov599 <abilovv599@gmail.com>
2024-05-05 10:48:01 +0400

    Refactor axios.ts to log baseURL in console

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
+console.log("baseURL", baseURL);
+
 const api = axios.create({ baseURL });
 
 api.interceptors.request.use((config) => {
