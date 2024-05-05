89522cc16fed378cdc8db7389b97f448f13e44ff
Abilov599 <abilovv599@gmail.com>
2024-05-05 09:35:06 +0400

    Refactor axios.ts to use NEXT_PUBLIC_API_URL from .env file

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-});
+const baseURL = process.env.NEXT_PUBLIC_API_URL;
+
+const api = axios.create({ baseURL });
 
 api.interceptors.request.use((config) => {
   const access_token =
