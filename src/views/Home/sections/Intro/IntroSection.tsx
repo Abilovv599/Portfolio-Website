78f36fdbb6c5f0554889907d3d1bfbaf0db15057
Abilov599 <abilovv599@gmail.com>
2024-12-12 09:44:44 +0400

    change resume

--- src/views/Home/sections/Intro/IntroSection.tsx
+++ src/views/Home/sections/Intro/IntroSection.tsx
@@ -89,10 +89,10 @@ const IntroSection: FC<IntroSectionProps> = ({ ...props }) => {
         </Link>
         <a
           className="borderBlack group flex cursor-pointer items-center gap-2 rounded-full bg-white px-7 py-3 outline-none transition hover:scale-110 active:scale-105 dark:bg-white/10"
-          href="/CV(Jeyhun_Abilov).pdf"
+          href="/Jeyhun-Abilov-SE-Resume.pdf"
           download
         >
-          Download CV{" "}
+          Download Resume{" "}
           <HiDownload className="opacity-60 transition group-hover:translate-y-1" />
         </a>
         <span className="flex gap-2">
@@ -105,7 +105,7 @@ const IntroSection: FC<IntroSectionProps> = ({ ...props }) => {
           </a>
           <a
             className="borderBlack flex h-[53.59px] w-[53.59px] cursor-pointer items-center justify-center gap-2 rounded-full bg-white p-4 text-[1.35rem] text-gray-700 transition hover:scale-[1.15] hover:text-gray-950 active:scale-105 dark:bg-white/10 dark:text-white/60"
-            href="https://github.com/Abilov599"
+            href="https://github.com/Abilovv599"
             target="_blank"
           >
             <FaGithubSquare />
